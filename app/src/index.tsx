fc9e3e527c52e64e1b391fa973b997c9eda8889d
Tiago Fassoni <tiagofassoni@gmail.com>
2022-07-19 17:33:51 +0200

    Adds the React part of the validator

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {validateEquation} from 'equation-validator';
+
+interface IEquationState {
+    value: string;
+}
+
+class EquationForm extends React.Component<{}, IEquationState> {
+    constructor() {
+        super({});
+        this.state = {value: '2 + 3 = 5'};
+
+    }
+
+    handleChange(event: any) {
+        console.dir(event)
+        this.setState({value: event.target.value});
+
+    }
+
+    render() {
+        return (
+            <p>Is <input className="equation" type="text" value={this.state.value}
+                         onChange={this.handleChange.bind(this)}></input> &nbsp;
+                <span>{String(validateEquation(this.state.value) === "OK" ? "Valid" : "Invalid")}</span>
+            </p>
+        );
+    }
+}
 
 ReactDOM.render(
     <React.StrictMode>
         <h1>Equation validator</h1>
-        <p>Is <span className='equation'>1 + 2 = 3</span> valid?</p>
+        <EquationForm/>
     </React.StrictMode>,
     document.getElementById('root')
 );
